30b8ced337fae2495980b957e6aea718624203fa
lvdkooi <lvvdkooi@hotmail.com>
2024-05-15 17:27:52 +0200

    Full implementation of the Conditional (article 2)

--- conditional/src/main/java/nl/kooi/Conditional.java
+++ conditional/src/main/java/nl/kooi/Conditional.java
@@ -1,43 +1,87 @@
 package nl.kooi;
 
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
-public class Conditional<S, T> {
+public final class Conditional<S, T> {
+
+    private final S value;
+    private final List<ConditionalAction<S, T>> conditionalActions;
+
+
+    private Conditional(S value,
+                        List<ConditionalAction<S, T>> actions) {
+        this.value = value;
+        this.conditionalActions = actions;
+    }
 
     public static <S> Conditional<S, S> of(S value) {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        return new Conditional<>(value, Collections.emptyList());
+    }
+
+    private static <S, T> Conditional<S, T> empty() {
+        return new Conditional<>(null, Collections.emptyList());
     }
 
     public static <S, U> ConditionalAction<S, U> applyIf(Predicate<S> condition, Function<S, U> function) {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        return new ConditionalAction<>(condition, function);
     }
 
     @SafeVarargs
     public final <U> Conditional<S, U> firstMatching(ConditionalAction<S, U>... actions) {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        var actionsAsList = Arrays.stream(actions).toList();
+
+        return new Conditional<>(value, actionsAsList);
     }
 
     public <U> Conditional<S, U> map(Function<T, U> mapFunction) {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        Objects.requireNonNull(mapFunction);
+
+        var updatedConditionalActions = conditionalActions
+                .stream()
+                .map(condAction -> condAction.and(mapFunction))
+                .toList();
+
+        return new Conditional<>(value, updatedConditionalActions);
     }
 
     public <U> Conditional<T, U> flatMap(Function<T, Conditional<T, U>> flatMapFunction) {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        return map(flatMapFunction)
+                .orElseGet(Conditional::empty);
     }
 
     public T orElseGet(Supplier<? extends T> supplier) {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        Objects.requireNonNull(supplier);
+
+        return Optional.ofNullable(value)
+                .flatMap(this::findMatchingFunction)
+                .orElseGet(() -> obj -> supplier.get())
+                .apply(value);
     }
 
     public T orElse(T defaultValue) {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        return Optional.ofNullable(value)
+                .flatMap(this::findMatchingFunction)
+                .orElse(obj -> defaultValue)
+                .apply(value);
     }
 
-    public <X extends Throwable> T orElseThrow(Supplier<? extends X> throwableSupplier) throws X {
-        throw new UnsupportedOperationException("Not yet implemented!");
+    public <X extends Throwable> T orElseThrow(Supplier<? extends X>
+                                                       exceptionSupplier) throws X {
+        Objects.requireNonNull(exceptionSupplier);
+
+        return Optional.ofNullable(value)
+                .flatMap(this::findMatchingFunction)
+                .orElseThrow(exceptionSupplier).apply(value);
+    }
+
+    private Optional<Function<S, T>> findMatchingFunction(S value) {
+        return conditionalActions.stream()
+                .filter(entry -> entry.condition().test(value))
+                .findFirst()
+                .map(ConditionalAction::action);
     }
 
     public record ConditionalAction<S, T>(Predicate<S> condition, Function<S, T> action) {
@@ -45,6 +89,11 @@ public class Conditional<S, T> {
         public ConditionalAction {
             Objects.requireNonNull(condition);
             Objects.requireNonNull(action);
+        }
+
+        public <U> ConditionalAction<S, U> and(Function<T, U> extraAction) {
+            return new ConditionalAction<>(condition,
+                    action.andThen(extraAction));
         }
     }
 }
